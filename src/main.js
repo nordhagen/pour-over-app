b638e79ac6d61b932dbd8d4fb0ca263bac7e2e50
nordhagen <oyvind.nordhagen@gmail.com>
2019-10-15 15:24:39 +0200

    introduce global css and window height logic

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import Vue from "vue";
-import App from "./App";
-import store from './Store';
+import Vue from 'vue'
+import App from './App'
+import store from './Store'
+import './global.css'
+import 'normalize.css'
 
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
+  el: '#app',
+  methods: {
+    resize: function() {
+      document.body.style = `height: ${window.innerHeight}px`
+    }
+  },
+  created: function() {
+    this.resize()
+    window.addEventListener('resize', this.resize)
+  },
   render: h => h(App),
   store
-});
+})
