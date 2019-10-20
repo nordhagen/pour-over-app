590c1f542ca8494ca2487f2741040194055739aa
nordhagen <oyvind.nordhagen@gmail.com>
2019-10-21 01:31:32 +0300

    implement changing of brew amount in store

--- src/components/BrewAmountStep.vue
+++ src/components/BrewAmountStep.vue
@@ -1,6 +1,13 @@
 <template>
   <section class="brew-step">
     <h2>How much coffee are you brewing?</h2>
+    <div class="button-container">
+      <button class="amount-button" @click="setBrewVolume(200)">1 cup/200 ml</button>
+      <button class="amount-button" @click="setBrewVolume(400)">2 cups/400 ml</button>
+      <button class="amount-button" @click="setBrewVolume(600)">3 cups/600 ml</button>
+      <button class="amount-button" @click="setBrewVolume(500)">1/2 liter</button>
+      <button class="amount-button" @click="setBrewVolume(1000)">1 liter</button>
+    </div>
     <nav class="bottom-nav"><button class="btn next-btn"><span class="btn-label">Continue</span></button></nav>
   </section>
 </template>
@@ -9,20 +16,28 @@
 export default {
   name: "BrewAmountStep",
   computed: {
-    volume(){
-      return this.$store.state.volume;
-    },
-    beanWeight(){
-      return this.$store.getters.beanWeight;
-    },
-    ratio(){
-      return this.$store.state.ratio;
-    }
+    volume: () => this.$store.state.volume
   },
   methods: {
-    updateVolume(e){
-      this.$store.commit('updateVolume', e.target.value)
+    setBrewVolume(volume){
+      this.$store.commit('updateBrewVolume', volume)
     }
   }
 };
 </script>
+
+<style lang="postcss" scoped>
+
+.button-container {
+  text-align: center;
+}
+.amount-button {
+  background: #eee;
+  border: none;
+  border-radius: 0;
+  width: 100px;
+  padding: 15px;
+  margin-bottom: 10px;
+  height: 100px;
+}
+</style>
